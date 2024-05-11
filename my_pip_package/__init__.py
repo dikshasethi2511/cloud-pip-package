b23a4247bbe389f5667549d53a20b2c6981ec7b3
Diksha Sethi <dikshasethi251@gmail.com>
2024-05-12 01:28:14 +0530

    testing out pip package install

--- my_pip_package/__init__.py
+++ my_pip_package/__init__.py
@@ -1,14 +1,14 @@
-import file_service_pb2 as your_proto_file_pb2
-import file_service_pb2_grpc as your_proto_file_pb2_grpc
+import file_service_pb2
+import file_service_pb2_grpc
 import grpc
 
 
 def get_file(peer_ip, peer_port, filepath):
     channel = grpc.insecure_channel(f"{peer_ip}:{peer_port}")
-    stub = your_proto_file_pb2_grpc.FileServiceStub(channel)
+    stub = file_service_pb2_grpc.FileServiceStub(channel)
     response = stub.GetFile(
-        your_proto_file_pb2.GetFileRequest(
-            address=your_proto_file_pb2.Address(IP=peer_ip, port=peer_port),
+        file_service_pb2.GetFileRequest(
+            address=file_service_pb2.Address(IP=peer_ip, port=peer_port),
             file_path=filepath,
         )
     )
@@ -17,10 +17,10 @@ def get_file(peer_ip, peer_port, filepath):
 
 def set_file(peer_ip, peer_port, filepath, content):
     channel = grpc.insecure_channel(f"{peer_ip}:{peer_port}")
-    stub = your_proto_file_pb2_grpc.FileServiceStub(channel)
+    stub = file_service_pb2_grpc.FileServiceStub(channel)
     response = stub.SetFile(
-        your_proto_file_pb2.SetFileRequest(
-            address=your_proto_file_pb2.Address(IP=peer_ip, port=peer_port),
+        file_service_pb2.SetFileRequest(
+            address=file_service_pb2.Address(IP=peer_ip, port=peer_port),
             file_path=filepath,
             file_content=content,
         )
@@ -30,10 +30,10 @@ def set_file(peer_ip, peer_port, filepath, content):
 
 def delete_file(peer_ip, peer_port, filepath):
     channel = grpc.insecure_channel(f"{peer_ip}:{peer_port}")
-    stub = your_proto_file_pb2_grpc.FileServiceStub(channel)
+    stub = file_service_pb2_grpc.FileServiceStub(channel)
     response = stub.DeleteFile(
-        your_proto_file_pb2.DeleteRequest(
-            address=your_proto_file_pb2.Address(IP=peer_ip, port=peer_port),
+        file_service_pb2.DeleteRequest(
+            address=file_service_pb2.Address(IP=peer_ip, port=peer_port),
             file_path=filepath,
         )
     )
