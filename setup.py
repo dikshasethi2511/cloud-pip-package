91cc0c04c3934bf8728c3a103b95b04c904d7171
Diksha Sethi <dikshasethi251@gmail.com>
2024-05-12 01:33:49 +0530

    included protobuf in dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="my_pip_package",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
-    install_requires=[
-        "grpcio",
-    ],
+    install_requires=["grpcio", "protobuf"],
     author="Diksha Sethi",
     author_email="diksha20056@iiitd.ac.in",
     url="https://github.com/dikshasethi2511/cloud-pip-package",
